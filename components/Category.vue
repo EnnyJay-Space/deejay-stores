1c256c694195efc3a3528b84ba277a7110a422b0
ennyworld <ennyjay4you@gmail.com>
2023-11-29 01:17:45 +0100

    completed the category section

--- components/Category.vue
+++ components/Category.vue
@@ -4,70 +4,39 @@
         </h2>
         <p class="mb-4 text-center text-neutral-600 dark:text-neutral-200">Problems trying to resolve the conflict between
         </p>
-        <div class="lg:px-14 grid lg:grid-cols-2 gap-5">
+        <div class="px-6 lg:px-14 grid lg:grid-cols-2 gap-5">
             <div class="relative">
                 <img src="../assets/img/unsplash_Bd7gNnWJBkU.jpg" alt="category"
-                    class="w-full mt-auto bottom-0 left-0 object-cover">
-                <div class="absolute bg-[#2d8ac0a1] w-70 pr-20 bottom-0 left-0 p-14">
+                    class="w-full mt-auto bottom-0 left-0  object-cover">
+                <div class="absolute bg-[#2d8ac0a1] w-70 pr-24  bottom-0 lg:left-0 p-8 lg:p-14">
                     <div class=" d-block">
-                        <p class="category-heading text-white mb-10">Top Product Of the Week</p>
-                        <NuxtLink class="border px-10 py-3 rounded text-white" to="/blog">EXPLORE ITEMS</NuxtLink>
+                        <p class="category-heading text-white mb-10 text-sm lg:text-2xl">Top Product Of the Week</p>
+                        <NuxtLink class="border px-10 py-3 rounded text-white text-sm" to="/blog">EXPLORE ITEMS</NuxtLink>
                     </div>
                 </div>
             </div>
-            <div class="grid gap-5">
-                <div class="relative h-24">
+            <div class="grid grid-cols-1 gap-8">
+                <div class="relative h-24 ">
+                    <img src="../assets/img/unsplash_MKvjc2kar7Q.jpg" alt="category"
+                        class="w-full mt-auto bottom-0 h-96 lg:h-72 left-0 object-cover ">
+                    <div class="absolute bg-[#2d8ac0a1] w-70 top-60  pr-24 lg:top-16 lg:mt-4 left-0 p-7 lg:p-14">
+                        <div class=" d-block">
+                            <p class="category-heading text-white mb-10 text-sm lg:text-2xl">Top Product Of the Week</p>
+                            <NuxtLink class="border px-10 py-3 rounded text-white text-sm " to="/blog">EXPLORE ITEMS</NuxtLink>
+                        </div>
+                    </div>
+                </div>
+                <div class="relative h-24 top-72 lg:top-0 bg-[#909]">
                     <img src="../assets/img/unsplash_muOHbrFGEQY.jpg" alt="category"
-                        class="w-full mt-auto h-80 bottom-0 left-0 object-cover">
-                    <div class="absolute bg-[#2d8ac0a1] w-70 pr-20 top-16 left-0 p-14 mt-3">
+                        class="w-full mt-auto bottom-0 h-96 lg:h-72 left-0 object-cover">
+                    <div class="absolute bg-[#2d8ac0a1] w-70 pr-24 top-60 lg:top-16 lg:mt-4 left-0 p-8 lg:p-14">
                         <div class=" d-block">
-                            <p class="category-heading text-white mb-10">Top Product Of the Week</p>
-                            <NuxtLink class="border px-10 py-3 rounded text-white" to="/blog">EXPLORE ITEMS</NuxtLink>
+                            <p class="category-heading text-white mb-10 text-sm lg:text-2xl">Top Product Of the Week</p>
+                            <NuxtLink class="border px-10 py-3 rounded text-white text-sm" to="/blog">EXPLORE ITEMS</NuxtLink>
                         </div>
                     </div>
                 </div>
-                <div class="relative h-24">
-                    <img src="../assets/img/unsplash_MKvjc2kar7Q.jpg" alt="category"
-                        class="w-full mt-auto h-72 bottom-0  left-0 object-cover">
-                    <!-- <div class="absolute bg-[#2d8ac0a1] w-70 pr-20 bottom-0 left-0 p-14">
-                        <div class=" d-block">
-                            <p class="category-heading text-white mb-10">Top Product Of the Week</p>
-                            <NuxtLink class="border px-10 py-3 rounded text-white" to="/blog">EXPLORE ITEMS</NuxtLink>
-                        </div>
-                    </div> -->
-                </div>
-                <!-- <div class="relative h-64">
-                    <img src="../assets/img/unsplash_MKvjc2kar7Q.jpg" alt="category"
-                        class="w-full mt-auto bottom-0  left-0 object-fill">
-                    <div class="absolute bg-[#2d8ac0a1] w-70 pr-20 bottom-0 left-0 p-14">
-                        <div class=" d-block">
-                            <p class="category-heading text-white mb-10">Top Product Of the Week</p>
-                            <NuxtLink class="border px-10 py-3 rounded text-white" to="/blog">EXPLORE ITEMS</NuxtLink>
-                        </div>
-                    </div>
-                </div> -->
-                <!-- <div class="relative">
-                    <img src="../assets/img/unsplash_Bd7gNnWJBkU.jpg" alt="category"
-                        class="w-full mt-auto bottom-0 left-0 object-cover">
-                    <div class="absolute bg-[#2d8ac0a1] w-70 pr-20 bottom-0 left-0 p-14">
-                        <div class=" d-block">
-                            <p class="category-heading text-white mb-10">Top Product Of the Week</p>
-                            <NuxtLink class="border px-10 py-3 rounded text-white" to="/blog">EXPLORE ITEMS</NuxtLink>
-                        </div>
-                    </div>
-                </div> -->
             </div>
-            <!-- <div class="card">
-                <img src="../assets/img/card-item.png" alt="category" class="w-full h-32 sm:h-48 object-cover">
-                <div class="m-4">
-                    <span class="font-bold">5 bean chill</span>
-                    <span class="block text-gray-500 text-sm">Recipe by EnnyJay</span>
-                </div>
-                <div class="badge">
-                    <span>25 mins</span>
-                </div>
-            </div> -->
-
         </div>
 
     </div>
@@ -87,7 +56,7 @@ export default {
 
 <style scoped>
 .category-heading {
-    font-size: 24px;
+    /* font-size: 24px; */
     font-family: 'Poppins', sans-serif;
     font-weight: 700;
 }
